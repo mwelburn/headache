c387049638908c7fb1ca47dc94568733c0bfa969
br0nc080 <mwelbu2@gmail.com>
2011-11-11 08:58:13 -0600

    Initial project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Headache::Application.config.secret_token = '6548f449015ec4e235ee0c8df79c3e68e6c6ad981085d8e49e117e67b43681de699f35ab60d7bdf5bcf50962e642a0d1f58cbe2f54ba3c235e8f94e1ae9fb2aa'
